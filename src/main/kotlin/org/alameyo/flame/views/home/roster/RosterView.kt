a8f70af0e8171a9ae55ce785a168c568aca954cc
Alameyo <alameyo@users.noreply.github.com>
2019-06-08 12:59:12 +0200

    Add button for add friend to roster

--- src/main/kotlin/org/alameyo/flame/views/home/roster/RosterView.kt
+++ src/main/kotlin/org/alameyo/flame/views/home/roster/RosterView.kt
@@ -1,8 +1,11 @@
 package org.alameyo.flame.views.home.roster
 
+import javafx.scene.control.Button
+import javafx.scene.image.ImageView
 import org.alameyo.flame.controllers.FlameController
 import org.alameyo.flame.controllers.chat.ChatAreaController
 import org.alameyo.flame.css.FlameStyle
+import org.alameyo.flame.css.FlameStyle.Companion.rosterList
 import org.alameyo.flame.models.FlameContactEntry
 import tornadofx.*
 
@@ -14,11 +17,22 @@ class RosterView : View() {
 
     override val root = scrollpane {
         vbox {
+            hbox {
+                addClass(rosterList)
+                button {
+                    val image = imageview("UI/Friend_Add.png")
+                    fitImageToButton(image, this)
+                    addClass(FlameStyle.roundButton)
+                    action {
+
+                    }
+                }
+            }
             rosterController.entries().forEach {
                 val flameRosterEntry = FlameContactEntry(it)
                 chatAreaController.addChat(flameRosterEntry)
                 hbox {
-                    addClass(FlameStyle.rosterList)
+                    addClass(rosterList)
                     button(it.name ?: it.jid.toString()) {
                         addClass(FlameStyle.roundButton)
                         action {
@@ -28,5 +42,10 @@ class RosterView : View() {
                 }
             }
         }
+    }
+
+    private fun fitImageToButton(imageView: ImageView, button: Button) {
+        imageView.fitHeightProperty().bind(button.heightProperty())
+        imageView.fitWidthProperty().bind(button.widthProperty())
     }
 }
